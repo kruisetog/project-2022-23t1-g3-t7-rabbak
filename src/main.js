f5a1f3f39c10a93be5433b8fe873b33a48fbbda7
yyiiinn <33303522+yyiiinn@users.noreply.github.com>
2022-10-30 00:13:56 +0800

    edited identity pool id

--- src/main.js
+++ src/main.js
@@ -4,17 +4,17 @@ import router from "./router";
 import {Amplify, Storage} from 'aws-amplify';
 
 Amplify.configure({
-    Auth:{
-        identityPoolID: 'us-east-1:a914bc24-32e4-412e-89fc-e16d34322aa8',
-        region: 'us-east-1'
+    Auth: {
+        identityPoolId: 'us-east-1:a914bc24-32e4-412e-89fc-e16d34322aa8', //REQUIRED - Amazon Cognito Identity Pool ID
+        region: 'us-east-1', // REQUIRED - Amazon Cognito Region
     },
-    Storage:{
-        AWSS3:{
-            bucket: 'batchuploadbucket',
-            region: 'us-east-1'
+    Storage: {
+        AWSS3: {
+            bucket: 'batchuploadbucket', 
+            region: 'us-east-1', 
         }
     }
-})
+});
 
 const app = createApp(App);
 

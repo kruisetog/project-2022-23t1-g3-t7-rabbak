269127c9c839a45c81808af3738e87b064006adf
yyiiinn <33303522+yyiiinn@users.noreply.github.com>
2022-11-03 14:23:15 +0800

    import.meta.env

--- src/main.js
+++ src/main.js
@@ -5,13 +5,13 @@ import {Amplify, Storage} from 'aws-amplify';
 
 Amplify.configure({
     Auth: {
-        identityPoolId: process.env.VUE_APP_IDENTITYPOOLID, 
-        region: process.env.VUE_APP_REGION,
+        identityPoolId: import.meta.env.VUE_APP_IDENTITYPOOLID, 
+        region: import.meta.env.VUE_APP_REGION,
     },
     Storage: {
         AWSS3: {
-            bucket: process.env.VUE_APP_BUCKETNAME, 
-            region: process.env.VUE_APP_REGION, 
+            bucket: import.meta.env.VUE_APP_BUCKETNAME, 
+            region: import.meta.env.VUE_APP_REGION, 
         }
     }
 });
